3d52ce1ba975441ba14392417c86f84ee6b659e6
zivmaor <ziv.maor06@gmail.com>
2020-04-18 17:23:31 +0300

    Added linting checks on travis (fixed long lines).

--- open_captcha/captcha_generator.py
+++ open_captcha/captcha_generator.py
@@ -6,7 +6,10 @@ from typing import Mapping, Sequence, Tuple
 import Levenshtein
 import pandas as pd
 
-from .common_types import RNG, InputTable, TemplateConfig, ChallengeId, Challenge, ServerContext, RenderingOptions
+from .common_types import (
+    RNG, InputTable, TemplateConfig, ChallengeId, Challenge, ServerContext,
+    RenderingOptions,
+)
 from .challenge_templates import instantiate_templates
 
 
@@ -23,9 +26,12 @@ def _verify_timeout(t0: int, timeout: int) -> bool:
     return elapsed_time <= timeout
 
 
-def _verify_text_is_close(correct_answer: str, user_answer: str, num_letters_per_allowed_typo: int) -> bool:
+def _verify_text_is_close(correct_answer: str,
+                          user_answer: str,
+                          num_letters_per_allowed_typo: int) -> bool:
     distance = Levenshtein.distance(user_answer, correct_answer)
-    max_distance = math.ceil(len(correct_answer) / num_letters_per_allowed_typo)
+    max_distance = math.ceil(
+        len(correct_answer) / num_letters_per_allowed_typo)
     return distance <= max_distance
 
 
@@ -37,13 +43,17 @@ class CaptchaGenerator:
                  num_letters_per_allowed_typo: int = 5,
                  rng_seed: int = None,  # Use for testing only
                  verify_config: bool = True):
-        self.data = {name: pd.DataFrame.from_records(table) for name, table in data.items()}
+        self.data = {
+            name: pd.DataFrame.from_records(table)
+            for name, table in data.items()
+        }
         self.templates = instantiate_templates(template_configs)
         self.response_timeout_sec = response_timeout_sec
         self.num_letters_per_allowed_typo = num_letters_per_allowed_typo
         self._non_crypto_rng = RNG(rng_seed)
 
-        # Catch configuration errors early (at config development time by server side programmer)
+        # Catch configuration errors early (at config development time by
+        # server side programmer)
         if verify_config:
             for t in self.templates:
                 t.generate_challenge(self.data, self._non_crypto_rng)
@@ -54,13 +64,18 @@ class CaptchaGenerator:
                            ) -> Tuple[ChallengeId, Challenge, ServerContext]:
         challenge_id = _generate_challenge_id()
         template = self._non_crypto_rng.choice(self.templates)
-        challenge, correct_answer = template.generate_challenge(self.data, self._non_crypto_rng, rendering_options)
-        context = ServerContext(_get_timestamp(), attempt_number, correct_answer)
+        challenge, correct_answer = template.generate_challenge(
+            self.data, self._non_crypto_rng, rendering_options)
+        context = ServerContext(_get_timestamp(),
+                                attempt_number, correct_answer)
         return challenge_id, challenge, context
 
-    def verify_response(self, user_answer: str, context: ServerContext) -> bool:
+    def verify_response(self,
+                        user_answer: str,
+                        context: ServerContext) -> bool:
         if not _verify_timeout(context.timestamp, self.response_timeout_sec):
             return False
-        if not _verify_text_is_close(context.correct_answer, user_answer, self.num_letters_per_allowed_typo):
+        if not _verify_text_is_close(context.correct_answer, user_answer,
+                                     self.num_letters_per_allowed_typo):
             return False
         return True
--- open_captcha/challenge_templates.py
+++ open_captcha/challenge_templates.py
@@ -5,7 +5,8 @@ from typing import Sequence, Tuple, Mapping, Type
 import matplotlib.figure
 
 from .common_types import (
-    TemplateConfig, ConfigurationError, Challenge, CaptchaError, DataTables, RNG, RenderingOptions
+    TemplateConfig, ConfigurationError, Challenge, CaptchaError, DataTables,
+    RNG, RenderingOptions
 )
 
 
@@ -32,17 +33,22 @@ class ChallengeTemplate(ABC):
         pass  # pragma: no cover
 
 
-def get_class_by_name_mapping() -> Mapping[str, Type[ChallengeTemplate]]:
+TemplateClassNameMapping = Mapping[str, Type[ChallengeTemplate]]
+
+
+def get_class_by_name_mapping() -> TemplateClassNameMapping:
     class_by_name = {}
     for cls in ChallengeTemplate.__subclasses__():
         name = cls.config_name
         if name in class_by_name:
-            raise CaptchaError(f'Config name {name} used by both {class_by_name[name].__name__} and {cls.__name__}')
+            raise CaptchaError(
+                f'Config name {name} used by both '
+                f'{class_by_name[name].__name__} and {cls.__name__}')
         class_by_name[name] = cls
     return class_by_name
 
 
-def instantiate_one_template(cls_by_name: Mapping[str, Type[ChallengeTemplate]],
+def instantiate_one_template(cls_by_name: TemplateClassNameMapping,
                              config: TemplateConfig) -> ChallengeTemplate:
     name, params = config
     try:
@@ -50,17 +56,23 @@ def instantiate_one_template(cls_by_name: Mapping[str, Type[ChallengeTemplate]],
     except KeyError:
         raise UnknownTemplate(name)
     try:
-        # This call may do additional type checks and raise BadTemplateParameters directly,
-        # but we must catch wrong number or names of parameters here.
+        # This call may do additional type checks and raise
+        # BadTemplateParameters directly, but we must catch wrong number or
+        # names of parameters here.
         template = cls(**params)
     except TypeError as ex:
-        raise BadTemplateParameters(f'Wrong parameters passed to template {name}: {ex}')
+        raise BadTemplateParameters(
+            f'Wrong parameters passed to template {name}: {ex}')
     return template
 
 
-def instantiate_templates(configs: Sequence[TemplateConfig]) -> Sequence[ChallengeTemplate]:
+def instantiate_templates(configs: Sequence[TemplateConfig]
+                          ) -> Sequence[ChallengeTemplate]:
     cls_by_name = get_class_by_name_mapping()
-    templates = [instantiate_one_template(cls_by_name, config) for config in configs]
+    templates = [
+        instantiate_one_template(cls_by_name, config)
+        for config in configs
+    ]
     if not templates:
         raise ConfigurationError('No templates configured')
     return templates
@@ -90,11 +102,12 @@ def render_bar_chart(label_value_pairs: Sequence[Tuple[str, float]],
 # Concrete template types
 #################################################################
 class MinMaxBarTemplate(ChallengeTemplate):
-    """Show several values with their associated labels as a bar chart. Ask for the label of the highest/lowest value.
+    """Show several values with their associated labels as a bar chart. Ask for
+     the label of the highest/lowest value.
 
     Example Config (usually as JSON string):
     ["bar", {
-      "question": "These {n} cities had the most reported symptoms yesterday. Which city reported the most symptoms?",
+      "question": "Which of these {n} cities had the most symptoms yesterday?",
       "table": "report_counts",
       "labels": "city_name",
       "values": "num_symptoms",
@@ -104,16 +117,19 @@ class MinMaxBarTemplate(ChallengeTemplate):
     """
     config_name = 'min-max-bar'
 
-    def __init__(self, question: str, table: str, labels: str, values: str, variant: str, n: int = 3):
+    def __init__(self, question: str, table: str, labels: str, values: str,
+                 variant: str, n: int = 3):
         try:
             self.question = question.format(n=n)
         except Exception:
-            raise ConfigurationError('The question can only contain placeholders for the parameter "n"')
+            raise ConfigurationError('The question can only contain'
+                                     ' placeholders for the parameter "n"')
         self.table_name = table
         self.label_column = labels
         self.value_column = values
         if variant.lower() not in {'min', 'max'}:
-            raise ConfigurationError(f'variant must be either "min" or "max". Got {variant}')
+            raise ConfigurationError(
+                f'variant must be either "min" or "max". Got {variant}')
         self.is_max = variant.lower() == 'max'
         self.n = n
 
